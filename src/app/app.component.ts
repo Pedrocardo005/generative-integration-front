ef4a71e3b095899fb3f06e9e2f0861d8f7ae1392
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-10-31 06:49:55 -0300

    :arrow_up: refactor: Migra para standalone component
    By: Pedro da Conceicao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass'],
+    standalone: true,
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
 })
 export class AppComponent {
   title = 'generative-integration';
