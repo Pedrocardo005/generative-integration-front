b6656bf0f78bfb070c9ece344b80b97d3be3064f
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-11-05 08:04:51 -0300

    :heavy_plus_sign: build: Add angular material no projeto
     - Add skeleton load
    
    By: Pedro da Conceicao
    Refs: #5

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+    providers: [importProvidersFrom(BrowserModule, AppRoutingModule), provideAnimationsAsync()]
 })
   .catch(err => console.error(err));
+ 
