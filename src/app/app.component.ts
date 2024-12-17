d2d4352f0c5a7f5942c9802ea8475631ae4f5dab
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-12-17 09:57:04 -0300

    :sparkles: feat: Integra com backend
    By: Pedro da Conceicao
    Refs: #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { ChatService } from './chat.service';
 import { ChatComponent } from './components/chat/chat.component';
 import { Chat } from './models/chat';
 
@@ -18,25 +19,22 @@ import { Chat } from './models/chat';
     MatIcon,
     ChatComponent,
   ],
+  providers: [ChatService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'generative-integration';
-  chats: Chat[] = [
-    {
-      text_input: 'Entrada',
-      gemini_output: 'Saída',
-    },
-    {
-      text_input: 'Entrada',
-      gemini_output: 'Saída',
-    },
-    {
-      text_input: 'Entrada',
-      gemini_output: 'Saída',
-    },
-    {
-      text_input: 'Entrada',
-      gemini_output: '',
-    },
-  ];
+  chats: Chat[] = [];
+
+  constructor(public chatService: ChatService) {}
+
+  ngOnInit(): void {
+    this.chatService.getAllChats().subscribe((chats) => (this.chats = chats));
+  }
+
+  sendQuestion(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.chatService.createChat(value).subscribe((chat) => {
+      this.chats.push(chat);
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-
-
+import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule), provideAnimationsAsync()]
-})
-  .catch(err => console.error(err));
- 
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+  ],
+}).catch((err) => console.error(err));
