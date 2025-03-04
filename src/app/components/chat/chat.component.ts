934dea12ccddb945b45d562f7ff1ac066633f01f
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-03-04 12:06:32 -0300

    :lipstick: feat: Add interpretador de markdown
    By: Pedro Cardo
    Refs: #6

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { MarkdownModule, provideMarkdown } from 'ngx-markdown';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { Chat } from 'src/app/models/chat';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [NgxSkeletonLoaderModule],
+  imports: [NgxSkeletonLoaderModule, MarkdownModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.sass',
+  providers: [provideMarkdown()],
 })
 export class ChatComponent {
   @Input()
